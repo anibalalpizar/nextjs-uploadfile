4b819a5c51cfb0df93578f631d179cef6d4f012c
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-05-29 19:31:54 -0600

    react form

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,30 @@
+"use client";
+
+import { useState } from "react";
 
 function HomePage() {
-  return <div>Home Page</div>
+  const [file, setfile] = useState();
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!file) return;
+          console.log("uploading file");
+        }}
+      >
+        <label>Upload file:</label>
+        <input
+          type="file"
+          onChange={(e) => {
+            setfile(e.target.files[0]);
+          }}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
