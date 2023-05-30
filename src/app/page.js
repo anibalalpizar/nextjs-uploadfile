a4e846a160e1cc656595d90ee58aa0ff4db89f5e
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-05-29 20:22:57 -0600

    tailwindcss

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 
 function HomePage() {
   const [file, setfile] = useState();
@@ -9,36 +10,57 @@ function HomePage() {
     setfile(e.target.files[0]);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+
+    try {
+      const formData = new FormData();
+      formData.set("file", file);
+
+      // send file to server
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+      if (res.ok) {
+        console.log("File uploaded");
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          if (!file) return;
-
-          try {
-            const formData = new FormData();
-            formData.set("file", file);
-
-            // send file to server
-            const res = await fetch("/api/upload", {
-              method: "POST",
-              body: formData,
-            });
-            if (res.ok) {
-              console.log("File uploaded");
-            }
-            const data = await res.json();
-            console.log(data);
-          } catch (e) {
-            console.log(e);
-          }
-        }}
-      >
-        <label>Upload file:</label>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-zinc-950 p-5">
+        <h1 className="text-4xl text-center my-4">Upload a file</h1>
+        <form onSubmit={handleSubmit} className="">
+          <input
+            type="file"
+            onChange={handleFileChange}
+            className="bg-zinc-900 to-zinc-100 p-2 rounded block mb-2"
+          />
+          <button
+            type="submit"
+            className="bg-green-500 to-zinc-100 p-2 rounded block w-full disabled:opacity-50"
+            disabled={!file}
+          >
+            Upload
+          </button>
+        </form>
+        {file && (
+          <Image
+            src={URL.createObjectURL(file)}
+            className="w-64 h-64 object-cover mx-auto"
+            width={256}
+            height={256}
+            alt="preview"
+          />
+        )}
+      </div>
     </div>
   );
 }
